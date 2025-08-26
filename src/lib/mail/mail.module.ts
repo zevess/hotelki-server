16e5139a2f823bc6f851fa385234aebe3c4f6a57
zevess <gazetdinovvlad04@gmail.com>
2025-08-26 16:38:08 +0300

    removed mailer

--- src/lib/mail/mail.module.ts
+++ src/lib/mail/mail.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getMailerConfig } from 'src/config/mailer.config';
 
 
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: getMailerConfig,
-      inject: [ConfigService]
-    })
-  ],
   providers: [MailService],
 })
 export class MailModule { }
